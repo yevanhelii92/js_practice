d727e0cffdf77d846a69aad4dcf1650b63553f9d
o.yevanhelii <o.yevanhelii@gmail.com>
2021-12-04 18:06:06 +0200

    add_some_js_practice

---
+++ first_task.js
@@ -0,0 +1,52 @@
+const someArray = [
+    {
+        id: 1,
+        name: 'Alex'
+    },
+    {
+        id: 2,
+        name: "Elena"
+    },
+    {
+        id: 3,
+        name: "Svetlana"
+    }
+]
+
+
+// first realization using filter
+
+let usersId = []
+function filterId(item) {
+    
+  if (item.id !== 0) {
+    usersId.push(item.id)
+  }
+  return usersId
+}
+
+someArray.filter(filterId)
+console.log('first realization using filter',usersId)
+
+
+//  second realization using for 
+
+function filterByID(array) {
+    let users = []
+    for (let item in array) {
+        users.push(array[item].id)
+    } 
+    console.log('second realization using for ', users)
+    return users
+}
+
+filterByID(someArray)
+
+
+//  third realization using forEach
+
+let userIdsByForEach = []
+someArray.forEach((item)=> {
+    userIdsByForEach.push(item.id)
+})
+console.log('second realization using for ', userIdsByForEach)
---
+++ second_task.js
@@ -0,0 +1,5 @@
+const languages = ['Java', 'JavaScript', 'Python', 'Ruby'];
+
+const result = languages.filter(word => word.length > 4);
+
+console.log(result);
